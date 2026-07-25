18660bba96bbff073c815e049b32df0935eede76
devvrushabh <devvrushabh@users.noreply.github.com>
2026-07-25 12:37:59 +0530

    Fix gradlew script JAVACMD path for Linux runner

--- gradlew
+++ gradlew
@@ -1,3 +1,13 @@
 #!/bin/sh
 # Gradle wrapper script for Unix/Linux/macOS
-exec "$JAVACMD" "$@" -classpath "$CLASSPATH" org.gradle.wrapper.GradleWrapperMain
+DIRNAME=$(dirname "$0")
+APP_HOME=$(cd "$DIRNAME" && pwd)
+CLASSPATH="$APP_HOME/gradle/wrapper/gradle-wrapper.jar"
+
+if [ -n "$JAVA_HOME" ] ; then
+    JAVACMD="$JAVA_HOME/bin/java"
+else
+    JAVACMD="java"
+fi
+
+exec "$JAVACMD" -classpath "$CLASSPATH" org.gradle.wrapper.GradleWrapperMain "$@"
--- gradlew.bat
+++ gradlew.bat
@@ -1,16 +1,11 @@
-@rem Gradle wrapper script for Windows
 @if "%DEBUG%"=="" @echo off
 set DIRNAME=%~dp0
 if "%DIRNAME%"=="" set DIRNAME=.
-set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-if defined JAVA_HOME goto findJavaFromJavaHome
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
-if %ERRORLEVEL% equ 0 goto execute
-:findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
-:execute
-"%JAVA_EXE%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+if defined JAVA_HOME (
+    set JAVA_EXE="%JAVA_HOME%\bin\java.exe"
+) else (
+    set JAVA_EXE="java.exe"
+)
+%JAVA_EXE% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
